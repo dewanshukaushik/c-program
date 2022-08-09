deb154186fe2c721dfe7b8fe38fdd963adf0dd02
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2022-08-09 21:41:34 +0530

    division code added

--- divide.c
+++ divide.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 int main() {
 
-  int a1, b2, multi;
+  int a1, b2, divide;
 
   printf("Enter two number ");
-  
+
+    //Storing user input into variable num1 & num2
+  scanf("%d %d", &a1, &b2);
+
+  // Divide two input numbers
+  divide = a1 / b2;
+
+  printf("division of %d and %d is: %d", a1, b2, divide);
+  return 0;
 }
