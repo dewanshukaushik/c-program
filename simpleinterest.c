a44b464c31f0f431adcd790ebcaf548d7c5b61a5
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2022-08-23 20:51:04 +0530

    update

--- simpleinterest.c
+++ simpleinterest.c
@@ -2,5 +2,12 @@
 #include<conio.h>
  void main()
  {
-    int 
+    int P,R,T ;
+    float SI;
+    printf("ENTER THE PRINCIPLE VALUE");
+    printf("ENTER THE INTEREST RATE");
+    printf("ENTER TH TIME");
+    scanf("%d,%d,%d", &P &R &T);
+    SI=P*R*T/100;
+    
  }
