386e579c62ea0fa7a77c52da7dd9d398f728515b
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2022-08-31 19:15:30 +0530

    update

--- .vscode/swipeano.c
+++ .vscode/swipeano.c
@@ -5,9 +5,9 @@
    int A,B,C;
    printf( " Enter the two number ");
    scanf("%d %d",&A,&B);
-    A=B;
-    B=C;
-    C=A;
-   printf(" After swapping = %d,%d",&A,&B);
+    A=C;
+    B=A;
+    C=B;
+   printf(" After swapping=%d %d",&A,&B);
    getch();
  }
