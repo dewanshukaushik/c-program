a73e92012578243ec017fb4033c99949cba47b27
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2023-02-05 14:12:46 +0530

    Create bonus.c

---
+++ .vscode/bonus.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float balance,amount;
+    char gender;
+    printf("enter the gender ");
+    scanf("%c",&gender);
+     printf("Enter the  balance  ");
+     scanf("%f",&balance );
+    if(gender=='F' || gender=='f')
+       {
+       
+          if (balance>=5000)
+          {
+          amount=(5*balance)/100;
+          printf("bonus = %f ",amount );
+              
+          }
+          else {
+          amount=(2*balance)/100;
+          printf("bonus = %f ",amount );
+          }
+
+       }
+    else 
+    { 
+        amount=(2*balance)/100;
+          printf("bonus = %f ",amount );
+    }
+    getch();
+    
+}
