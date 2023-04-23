2f2d78e90538a0d93bafd37482ad0155eb9ba29c
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2023-04-23 16:23:54 +0530

    dsa

--- stack.c
+++ stack.c
@@ -1,12 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<conio.h>
-#define max=100
+#include <stdlib.h>
+#define Max 100
+void push(int stack[], int *top, int item);
+int pop(int stack[], int *top);
 void main()
-int stack[max],top=-1;
-{ 
+{
+    int stack[Max], top = -1, ch, item;
     while (1)
     {
-        switch
+        printf("\n1. Push ");
+        printf("\n2. Pop ");
+        printf("\n3. Exit");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            if (top == Max - 1)
+                printf("\nOverflow");
+            else
+            {
+                printf("\nEnter element ");
+                scanf("%d", &item);
+                push(stack, &top, item);
+            }
+            break;
+        case 2:
+            if (top == -1)
+                printf("\nUnderflow");
+            else
+            {
+                item = pop(stack, &top);
+                printf("%d", item);
+            }
+            break;
+        case 3: exit(0);
+        }
     }
-    
 }
+void push(int stack[], int *top, int item)
+{
+    (*top)++;
+    stack[*top] = item;
+}
+int pop(int stack[], int *top)
+{
+    int item;
+    item = stack[*top];
+    (*top)--;
+    return item;}
