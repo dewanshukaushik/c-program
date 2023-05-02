6a7a3fd0e40b527dd5e47abe609a454086c2301d
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2023-05-02 10:45:41 +0530

    practice

--- aqueue.c
+++ aqueue.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include<conio.h>
+#include <conio.h>
 #include <stdlib.h>
-void enqueue(int q[],int front,int rear,int item );
-void dequeue(int q[],int front,int rear,int item );
+void enqueue(int q[], int front, int rear, int item);
+void dequeue(int q[], int front, int rear, int item);
 void main()
 {
-    int  ch, item;
+    int ch, item;
     while (1)
     {
         printf("\n1.insertion ");
@@ -15,49 +15,44 @@ void main()
         switch (ch)
         {
         case 1:
-                void enqueue( q[], front,rear,item );
+            void enqueue(q[], front, rear, item);
             break;
         case 2:
-           
-                 void dequeue(int q[],int front,int rear,int item );
+
+            void dequeue(int q[], int front, int rear, int item);
             break;
-        case 3: exit(0);
+        case 3:
+            exit(0);
         }
     }
 }
-void enqueue(int q[],int front,int rear,int item );
+void enqueue(int q[], int front, int rear, int item);
 {
-    if((front==0&&rear==n-1)||(front==rear+1))
-    printf("overflow");
+    if ((front == 0 && rear == n - 1) || (front == rear + 1))
+        printf("overflow");
     else
     {
-        if(front==-1)
-        front=rear=0;
-        else if (rear==n-1)
-        rear =  0;
-        else 
-        rear++;
-    
+        if (front == -1)
+            front = rear = 0;
+        else if (rear == n - 1)
+            rear = 0;
+        else
+            rear++;
     }
-    q[rear]=item;
-    
-
+    q[rear] = item;
 }
-oid dequeue(int q[],int front,int rear,int item );
+oid dequeue(int q[], int front, int rear, int item);
 {
-    if(front==-1)
-    printf("underflow");
+    if (front == -1)
+        printf("underflow");
     else
     {
-        if(front==rear)
-        front=rear=-1;
-        else if (front==n-1)
-        front =  0;
-        else 
-        front++;
-    
+        if (front == rear)
+            front = rear = -1;
+        else if (front == n - 1)
+            front = 0;
+        else
+            front++;
     }
-    item=q[front];
-    
-
+    item = q[front];
 }
