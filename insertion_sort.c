fd820f85c93065bb6f81f0c3b4c1f86e0e58a3c3
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2023-05-05 10:13:49 +0530

    update

---
+++ insertion_sort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <conio.h>
+void insertion_sort(int[], int);
+void inputarray(int[], int);
+void outputarray(int[], int);
+void main()
+{
+    int a[20], n;
+    printf("enter the size of array :\n");
+    scanf("%d", &n);
+    inputarray(a, n);
+    insertion_sort(a, n);
+
+    outputarray(a, n);
+}
+void inputarray(int a[], int n)
+{
+    int i;
+    printf("Enter the array");
+    for (i = 0; i < n; i++)
+    
+    {
+        scanf("%d", &a[i]);
+    }
+}
+void insertion_sort(int a[], int n)
+{
+    int i, j, temp;
+    for (i = 1; i < n; i++)
+    {
+        for (j = 0; j < n - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void outputarray(int a[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+}
